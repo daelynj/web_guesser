f2f2efa42b631afbefc44f9f314b832e74c40a36
Daelyn Jones <daelyn.jones@juul-daelynjones-mbt15.local>
2019-05-06 15:34:26 -0700

    use an ERB template and HMTL

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,5 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-    "The secret number is: #{rand(0..100)}"
+    #render the ERB template named index and create a local variable for the template named number 
+    #which has the same value as the number variable from this server code
+    erb :index, :locals => {:number => rand(100)}    
 end
