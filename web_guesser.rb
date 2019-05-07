3c0cb877f12f637f4da9c319dce19c14bdf85fb2
Daelyn Jones <daelyn.jones@juul-daelynjones-mbt15.local>
2019-05-07 10:15:06 -0700

    show user if their guess is too high or too low

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
+@@random_num = rand(0..100)
 get '/' do
+    guess = params["guess"].to_i
+    @message = check_guess(guess)
+
     #render the ERB template named index and create a local variable for the template named number 
     #which has the same value as the number variable from this server code
-    erb :index, :locals => {:number => rand(100)}    
+    erb :index, :locals => {:number  => @@random_num,
+                            :message => @message}
+end
+def check_guess(guess)
+    if (guess - @@random_num) > 5
+        "Way too high!"
+    elsif guess > @@random_num
+        "Too high!"
+    elsif (@@random_num - guess) < 5
+        "Way too low!"
+    elsif guess < @@random_num
+        "Too low!"
+    else guess == @@random_num
+        "Win"
+    end
+
 end
